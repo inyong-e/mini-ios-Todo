31b5971ca3ab3f7d161f91c679f61ca246498e3b
inyong-e <jiy8319@gmail.com>
2020-06-13 19:52:08 +0900

    Update: TitleList 컴포넌트를 Functional로 변경
    불 필요한 class 컴포넌트 사용을 없애고 성능, 코드 직관성으로 더 좋은 Functional 컴포넌트로 리팩토링을 했다.
    아직 React hooks는 useState, useEffect밖에 모르는지라, 아쉽게도 state 변경의 비동기 처리 부분(41 줄~)을 async await으로 처리했다..
    분명 React hooks에 비동기 처리가 가능한 방법이 있을테니, 이따 한번 찾아봐야겠다.

--- src/component/TitleList.js
+++ src/component/TitleList.js
@@ -1,109 +1,30 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class TitleList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isShowInputTitle: false,
-      isShowNotice: false,
-    };
-  }
+const TitleList = props => {
+  const {
+    todoItems,
+    selectedTitle,
+    onClickTitle,
+    searchKeyword,
+    changeSearchKeyword,
+  } = props;
 
-  onAddButton = async () => {
-    await this.setState({
-      isShowInputTitle: true,
-      isShowNotice: false,
-    });
+  const [isShowNotice, setIsShowNotice] = useState(false);
+  const [isShowInputTitle, setIsShowInputTitle] = useState(false);
 
-    document.getElementById("input-title-box").focus();
-  };
-
-  render() {
-    const {
-      todoItems,
-      selectedTitle,
-      searchKeyword,
-      changeSearchKeyword,
-    } = this.props;
-    const { isShowInputTitle, isShowNotice } = this.state;
-    return (
-      <div>
-        <header>
-          <input
-            type="text"
-            id="searchInputBox"
-            placeholder="검색"
-            onClick={this.clearText}
-            value={searchKeyword}
-            onChange={changeSearchKeyword}
-          />
-        </header>
-        iCloud
-        <div className="ListArea">
-          {todoItems.map(todoItem => (
-            <div
-              key={todoItem.title}
-              className="TitleEntry"
-              onClick={this.props.onClickTitle(todoItem.title)}
-              style={{
-                backgroundColor:
-                  todoItem.title === selectedTitle
-                    ? "rgb(167,167,167)"
-                    : "initial",
-              }}
-            >
-              {todoItem.title}
-            </div>
-          ))}
-          {isShowInputTitle && (
-            <input
-              id="input-title-box"
-              placeholder="새로운 목록"
-              onKeyUp={this.onEventKey}
-              onBlur={this.onClickAddBtn}
-            />
-          )}
-          {isShowNotice && (
-            <div
-              style={{
-                color: "rgb(190,48,27)",
-                fontSize: "15px",
-              }}
-            >
-              Already exists this Title..
-            </div>
-          )}
-        </div>
-        <div className="footer">
-          <button
-            onClick={this.onAddButton}
-            style={{
-              fontSize: "12px",
-              width: "240px",
-            }}
-          >
-            +add List
-          </button>
-        </div>
-      </div>
-    );
-  }
-
-  clearText = () => {
-    this.setState({
-      isShowNotice: false,
-    });
+  const clearText = () => {
+    setIsShowNotice(false);
     document.getElementById("searchInputBox").value = "";
   };
 
-  onEventKey = () => {
+  const onEventKey = () => {
     if (window.event.keyCode === 13) {
       document.activeElement.blur();
     }
   };
 
-  onClickAddBtn = e => {
-    const { todoItems } = this.props;
+  const onClickAddBtn = e => {
+    const { todoItems, onAddTodoItem } = props;
     const newTitle = e.currentTarget.value;
 
     const isEmpty = newTitle === "";
@@ -111,13 +32,79 @@ class TitleList extends Component {
       todoItems.findIndex(todoItem => todoItem.title === newTitle) >= 0;
 
     if (!isExistTitle && !isEmpty) {
-      this.props.onAddTodoItem(newTitle);
+      onAddTodoItem(newTitle);
     }
-    this.setState({
-      isShowInputTitle: false,
-      isShowNotice: isExistTitle,
-    });
+    setIsShowInputTitle(false);
+    setIsShowNotice(isExistTitle);
   };
-}
+
+  const onClickAddListBtn = async () => {
+    await setIsShowInputTitle(true);
+    await setIsShowNotice(false);
+
+    document.getElementById("input-title-box").focus();
+  };
+  return (
+    <div>
+      <header>
+        <input
+          type="text"
+          id="searchInputBox"
+          placeholder="검색"
+          onClick={clearText}
+          value={searchKeyword}
+          onChange={changeSearchKeyword}
+        />
+      </header>
+      iCloud
+      <div className="ListArea">
+        {todoItems.map(todoItem => (
+          <div
+            key={todoItem.title}
+            className="TitleEntry"
+            onClick={onClickTitle(todoItem.title)}
+            style={{
+              backgroundColor:
+                todoItem.title === selectedTitle
+                  ? "rgb(167,167,167)"
+                  : "initial",
+            }}
+          >
+            {todoItem.title}
+          </div>
+        ))}
+        {isShowInputTitle && (
+          <input
+            id="input-title-box"
+            placeholder="새로운 목록"
+            onKeyUp={onEventKey}
+            onBlur={onClickAddBtn}
+          />
+        )}
+        {isShowNotice && (
+          <div
+            style={{
+              color: "rgb(190,48,27)",
+              fontSize: "15px",
+            }}
+          >
+            Already exists this Title..
+          </div>
+        )}
+      </div>
+      <div className="footer">
+        <button
+          onClick={onClickAddListBtn}
+          style={{
+            fontSize: "12px",
+            width: "240px",
+          }}
+        >
+          +add List
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default TitleList;
