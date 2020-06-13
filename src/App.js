183956f260c8e3e07f679449f599635c3b4ebdec
inyong-e <jiy8319@gmail.com>
2020-06-13 15:54:58 +0900

    Update: Todo의 타이틀 리스트를 저장하는 state를 최상단으로 이동
    오른쪽 화면의 TodoList 컴포넌트에서 Todo의 타이틀 리스트를 가져올 때, getElementById DOM으로 접근하는 것을 없애기 위함.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
-import React, { Component } from 'react';
-import TitleList from './component/TitleList';
-import TodoList from './component/TodoList';
-import './css/App.css';
+import React, { Component } from "react";
+import TitleList from "./component/TitleList";
+import TodoList from "./component/TodoList";
+import "./css/App.css";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      currentTitle : '',
-      searchKeyword : ''
-    }
-    
+      titleList: [],
+      selectTitle: "",
+      searchKeyword: "",
+    };
   }
-  changeTitle = (name) => {
+
+  changeTitle = name => {
     this.setState({
-      currentTitle : name
-    })
-  }
-  changeSearchKeyword = (keyword) => {
+      selectTitle: name,
+    });
+  };
+  changeSearchKeyword = keyword => {
     this.setState({
-      searchKeyword: keyword
-    })
-  }
+      searchKeyword: keyword,
+    });
+  };
   render() {
+    const { titleList } = this.state;
     return (
       <div className="App">
-        <div className='TitleListArea'><TitleList changeTitle = {this.changeTitle} changeSearchKeyword = {this.changeSearchKeyword} currentTitle = {this.state.currentTitle} /></div>
-        
-        <div className='TodoListArea'><TodoList currentTitle={this.state.currentTitle} searchKeyword = {this.state.searchKeyword} changeTitle={this.changeTitle}/></div>
+        <div className="TitleListArea">
+          <TitleList
+            onAddTitle={this.onAddTitle}
+            titleList={titleList}
+            changeTitle={this.changeTitle}
+            changeSearchKeyword={this.changeSearchKeyword}
+            selectTitle={this.state.selectTitle}
+          />
+        </div>
+
+        <div className="TodoListArea">
+          <TodoList
+            selectTitle={this.state.selectTitle}
+            searchKeyword={this.state.searchKeyword}
+            changeTitle={this.changeTitle}
+          />
+        </div>
       </div>
     );
   }
+  onAddTitle = newTitle => {
+    this.setState({
+      titleList: [...this.state.titleList, newTitle],
+      selectTitle: newTitle,
+    });
+  };
 }
 
 export default App;
--- src/component/TitleList.js
+++ src/component/TitleList.js
@@ -1,88 +1,100 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-class TitleList extends Component{
-  constructor(props){
+class TitleList extends Component {
+  constructor(props) {
     super(props);
-    this.state = {
-      TitleList : []
-    }
   }
 
   addTitle = () => {
-
-    var eventVar = ()=>{
-      if(window.event.keyCode === 13){
+    var eventVar = () => {
+      if (window.event.keyCode === 13) {
         document.activeElement.blur();
       }
-    }
+    };
     var setNewTitleFunc = () => {
-      let newTitleName = document.getElementById('inputTitleTag').value;
-      if(this.state.TitleList.includes(newTitleName)){
-        document.getElementById('noticeArea').innerHTML = 'Already exists this Title.. ';
+      const { titleList } = this.props;
 
-      }else if(newTitleName !== ''){
-        this.setState({
-          TitleList: [...this.state.TitleList, newTitleName],
-        })
-        this.props.changeTitle(newTitleName);
+      let newTitle = document.getElementById("inputTitleTag").value;
+      if (titleList.includes(newTitle)) {
+        document.getElementById("noticeArea").innerHTML =
+          "Already exists this Title.. ";
+      } else if (newTitle !== "") {
+        this.props.onAddTitle(newTitle);
       }
-      document.getElementById('inputTitleTag').remove();
-    }
+      document.getElementById("inputTitleTag").remove();
+    };
 
-    document.getElementById('noticeArea').innerHTML = '';
-    document.getElementById('searchInputBox').value = '';
-    this.props.changeSearchKeyword('');
-    var input = document.createElement('input');
-    input.id = 'inputTitleTag';
+    document.getElementById("noticeArea").innerHTML = "";
+    document.getElementById("searchInputBox").value = "";
+    this.props.changeSearchKeyword("");
+    var input = document.createElement("input");
+    input.id = "inputTitleTag";
     input.onkeyup = eventVar;
-    input.placeholder = '새로운 목록'
-    input.onblur= setNewTitleFunc;
-    document.querySelector('.newinputArea').appendChild(input);
+    input.placeholder = "새로운 목록";
+    input.onblur = setNewTitleFunc;
+    document.querySelector(".newinputArea").appendChild(input);
     input.focus();
-    
-  }
-  selectTitleFunc = (title) => {
-    
+  };
+  selectTitleFunc = title => {
     this.clrearNoticeMessage();
     this.props.changeTitle(title);
-  }
+  };
   clrearNoticeMessage = () => {
-    document.getElementById('noticeArea').innerHTML='';
-    document.getElementById('searchInputBox').value = '';
-  }
-  render(){
-
+    document.getElementById("noticeArea").innerHTML = "";
+    document.getElementById("searchInputBox").value = "";
+  };
+  render() {
     const buttonStyle = {
-      fontSize : '12px',
-      width: '280px'
-    } 
+      fontSize: "12px",
+      width: "280px",
+    };
     const selectTitleStyle = {
-      backgroundColor : 'rgb(167,167,167)'
-    }
+      backgroundColor: "rgb(167,167,167)",
+    };
     const noticeAreaStyle = {
-      color : 'rgb(190,48,27)',
-      fontSize : '15px'
-    }
-    return(
+      color: "rgb(190,48,27)",
+      fontSize: "15px",
+    };
+    const { titleList } = this.props;
+    return (
       <div>
         <header>
-          <input type ='text' id='searchInputBox' placeholder='검색' onClick={this.clrearNoticeMessage} onChange={(e)=>{this.props.changeSearchKeyword(e.target.value)}}/>
+          <input
+            type="text"
+            id="searchInputBox"
+            placeholder="검색"
+            onClick={this.clrearNoticeMessage}
+            onChange={e => {
+              this.props.changeSearchKeyword(e.target.value);
+            }}
+          />
         </header>
         iCloud
-        
-        <div className='ListArea'>
-          {this.state.TitleList.map(
-            (title,index) => (<div key={index} className='TitleEntry' onClick={()=>{this.selectTitleFunc(title)}} style={title === this.props.currentTitle ? selectTitleStyle : {}}>{title}</div>)
-          )}
-          <div className='newinputArea'></div>
-          <div id='noticeArea' style={noticeAreaStyle}></div>
+        <div className="ListArea">
+          {titleList.map(title => (
+            <div
+              key={title}
+              className="TitleEntry"
+              onClick={this.onClickTitle(title)}
+              style={title === this.props.selectTitle ? selectTitleStyle : {}}
+            >
+              {title}
+            </div>
+          ))}
+          <div className="newinputArea"></div>
+          <div id="noticeArea" style={noticeAreaStyle}></div>
         </div>
-        <div className='footer'>
-          <button onClick={this.addTitle} style={buttonStyle}>+add List</button>
+        <div className="footer">
+          <button onClick={this.addTitle} style={buttonStyle}>
+            +add List
+          </button>
         </div>
       </div>
-    )
+    );
   }
+  onClickTitle = title => () => {
+    this.props.changeTitle(title);
+  };
 }
 
 export default TitleList;
